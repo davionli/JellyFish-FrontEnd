7a26971948c5ee1aff3228456bc5e8bbbb1df9d6
Aaron Miller <aaaronmiller@gmail.com>
2019-08-06 21:33:00 -0700

    color changes

--- boardgame/src/components/Navbar.js
+++ boardgame/src/components/Navbar.js
@@ -17,9 +17,9 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-                <MDBNavbar color="indigo" dark expand="md">
+                <MDBNavbar color="#283593 indigo darken-3" dark expand="md">
                     <MDBNavbarBrand>
-                        <strong className="white-text">BoardGame Parties</strong>
+                        <strong className="white-text title">BoardGames With "Friends"</strong>
                     </MDBNavbarBrand>
                     <MDBNavbarToggler onClick={this.toggleCollapse} />
                     <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
